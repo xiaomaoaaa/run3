86e7d2b5facd8c151e36de4428bb297e1f377209
PC\XT <abelmao@shinyway.com.cn>
2021-03-29 17:38:07 +0800

    修改了规则

--- src/views/runindex/runindexApp.vue
+++ src/views/runindex/runindexApp.vue
@@ -5,10 +5,11 @@
     <div class="form">
       <div class="logo"></div>
       <div class="runname"><img :src="huanlepaologo"/></div>
-      <h2>2019新通教育Forever Young城市欢乐跑</h2>
+      <h2>2021新通教育25周年</h2>
+      <h2>“520，I LOVE 游”爱心春游</h2>
       <h2>- 诚邀您参与 -</h2>
-      <p>活动时间：2019年5月26日 8:00-11:00</p>
-      <p>报名时间：即日起至2019年5月12日</p>
+      <p>活动时间：2021-4-18 上午9:00-11:00</p>
+      <p>报名时间：2021-04-06至2021-04-09</p>
       <a class="subbtn" href="runregister.html">立即报名</a>
       <div class="foottext">
          <a href="agreement.html">活动规则</a>
